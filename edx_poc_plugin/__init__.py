d15a1d66935bc71f5fbc6ce744b1a1319979f7e7
9vinny <9vinny@gmail.com>
2019-01-28 01:03:27 -0800

    version change

--- edx_poc_plugin/__init__.py
+++ edx_poc_plugin/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 default_app_config = 'edx_poc_plugin.apps.EdxPocPluginConfig'  # pylint: disable=invalid-name
